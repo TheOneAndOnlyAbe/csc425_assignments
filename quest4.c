fc9ed3287476277847701790a980ed050ea8a690
morenoa2 <morenoa2@csc4252021.ssdt.berea.edu>
2021-09-02 19:18:42 -0400

    Completed quest4

--- quest4.c
+++ quest4.c
@@ -6,23 +6,27 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-	// int rc = fork();
+	int rc = fork();
 
-	// testFileForExecuting
-
-	// execl("/bin/ls", "ls", NULL);
-	execle("/bin/ls", "ls", NULL);
-
-	/*
 	if (rc < 0) {
 	 	fprintf(stderr, "Fork failed\n");
 		exit(1);
 	} else if (rc == 0) {
 		printf("I am the child process and x is %d \n", x);
+
+		char const *envp[] = {NULL};
+	        char *args[] = {"/bin/ls", NULL};
+
+		// execl("/bin/ls", "ls", NULL);
+        	// execle("/bin/ls", "ls", NULL, *envp);
+        	// execlp("/bin/ls", "ls", NULL);
+        	// execv(args[0], args);
+        	// execvp(args[0], args);
+        	// execvpe("/bin/ls", args, envp); It says that this one does not exist...
+
 	} else {
 		printf("I am the parent process and x is %d \n", x);
 	}
-	*/
 
 	return 0;
 }
@@ -32,6 +36,8 @@ int main(int argc, char *argv[]) {
 
 Abraham Moreno
 
-STUCK
+Question 1: Maybe there are many variations so as to be able to execute
+	 the exact same thing in different situations or enviornments
+	 without having problems.
 
 ************************************************************************/
